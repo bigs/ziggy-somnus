2cf5faaf5bad5653a961177ca964cda215cca75e
Cole Brown <bigswim@gmail.com>
2024-09-26 22:28:36 -0400

    Add join command

--- src/commands.zig
+++ src/commands.zig
@@ -11,3 +11,11 @@ pub fn user(allocator: std.mem.Allocator, nickname: []const u8, mode: u8, realna
 pub fn pong(allocator: std.mem.Allocator, message: []const u8) ![]const u8 {
     return std.fmt.allocPrint(allocator, "PONG :{s}\r\n", .{message});
 }
+
+pub fn join(allocator: std.mem.Allocator, channel: []const u8, password: ?[]const u8) ![]const u8 {
+    if (password) |pass| {
+        return std.fmt.allocPrint(allocator, "JOIN {s} {s}\r\n", .{ channel, pass });
+    } else {
+        return std.fmt.allocPrint(allocator, "JOIN {s}\r\n", .{channel});
+    }
+}
